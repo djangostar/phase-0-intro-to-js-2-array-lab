a9f9646c6c4c30c23ca869af382bea0bba477afc
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2023-12-19 17:20:04 -0800

    Redone with comments

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
 // Write your solution here!
-
 const cats = ["Milo", "Otis", "Garfield"];
 
 const destructivelyAppendCat = (name) => {
-  return cats.push(name);
+  // appends a cat to the end of the cats array: destructively
+  cats.push(name);
 };
 
 const destructivelyPrependCat = (name) => {
-  return cats.unshift(name);
+  // prepends a cat to the beginning of the cats array: destructively
+  cats.unshift(name);
 };
 
 const destructivelyRemoveLastCat = () => {
-  return cats.pop();
+  // removes the last cat from the cats array: destructively
+  cats.pop();
 };
 
 const destructivelyRemoveFirstCat = () => {
-  return cats.shift();
+  // removes the first cat from the cats array: destructively
+  cats.shift();
 };
 
 const appendCat = (name) => {
+  // appends a cat to the cats array and returns a new array, leaving the cats array unchanged
   const newCats = [...cats, name];
   return newCats;
 };
 
 const prependCat = (name) => {
+  // prepends a cat to the cats array and returns a new array, leaving the cats array unchanged
   const newCats = [name, ...cats];
   return newCats;
 };
 
 const removeLastCat = () => {
+  // removes the last cat in the cats array and returns a new array, leaving the cats array unchanged
   return cats.slice(0, -1);
 };
 
 const removeFirstCat = () => {
+  // removes the first cat from the cats array and returns a new array, leaving the cats array unchanged
   return cats.slice(1);
 };
